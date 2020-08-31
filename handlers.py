4d6b0b4ac4f3debfe8c05f0972669bc1fede1969
noXplode <crazzygunit@gmail.com>
2020-09-01 01:49:35 +0300

    updated calendar, minor fixes

--- handlers.py
+++ handlers.py
@@ -1,11 +1,11 @@
-from aiogram import types
+from aiogram.types import Message, CallbackQuery, ReplyKeyboardMarkup
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 
 from bot import bot, dp
 from config import WEATHER_TOKEN, ADMIN_ID
-from aiogramcalendar import create_calendar, process_calendar_selection
+from aiogramcalendar import calendar_callback, create_calendar, process_calendar_selection
 
 from datetime import datetime
 import logging
@@ -28,13 +28,13 @@ class BotStates(StatesGroup):
     picked_rates_date = State()
 
 
-start_kb = types.ReplyKeyboardMarkup(resize_keyboard=True,)
+start_kb = ReplyKeyboardMarkup(resize_keyboard=True,)
 start_kb.row('Погода', 'Курс UAH/USD')
 
-rates_kb = types.ReplyKeyboardMarkup(resize_keyboard=True,)
+rates_kb = ReplyKeyboardMarkup(resize_keyboard=True,)
 rates_kb.row('Курс на сегодня', 'Курс на дату', 'Отмена')
 
-cancel_kb = types.ReplyKeyboardMarkup(resize_keyboard=True,)
+cancel_kb = ReplyKeyboardMarkup(resize_keyboard=True,)
 cancel_kb.row('Отмена')
 
 
@@ -44,7 +44,7 @@ async def send_to_admin(dp):
 
 # starting bot when user sends `/start` command
 @dp.message_handler(commands=['start'])
-async def cmd_start(message: types.Message):
+async def cmd_start(message: Message):
     await BotStates.started.set()
     await message.reply("Привет!\nВыбери что тебе интересно: погода или курс валют", reply_markup=start_kb)
 
@@ -52,7 +52,7 @@ async def cmd_start(message: types.Message):
 # cancel everything
 @dp.message_handler(state='*', commands='cancel')
 @dp.message_handler(Text(equals=['cancel', 'отмена'], ignore_case=True), state='*')
-async def cancel_handler(message: types.Message, state: FSMContext):
+async def cancel_handler(message: Message, state: FSMContext):
     current_state = await state.get_state()
     if current_state is None:
         return
@@ -65,7 +65,7 @@ async def cancel_handler(message: types.Message, state: FSMContext):
 
 # picking weather or currency rate
 @dp.message_handler(state=BotStates.started)
-async def first_pick(message: types.Message, state: FSMContext):
+async def first_pick(message: Message, state: FSMContext):
     if message.text == 'Погода':
         await BotStates.picked_weather.set()
         await message.answer("Введите город в котором нужна погода", reply_markup=cancel_kb)
@@ -78,7 +78,7 @@ async def first_pick(message: types.Message, state: FSMContext):
 
 # picked currency rate
 @dp.message_handler(state=BotStates.picked_rates)
-async def currency_pick(message: types.Message, state: FSMContext):
+async def currency_pick(message: Message, state: FSMContext):
     if message.text == 'Курс на сегодня':
         rate = await getcurrateuah()
         await message.answer(f'По данным НБУ курс UAH/USD сегодня {str(rate)}', reply_markup=start_kb)
@@ -90,20 +90,23 @@ async def currency_pick(message: types.Message, state: FSMContext):
         await message.answer("Выбери на какую дату нужен курс валют")
 
 
-@dp.callback_query_handler(state=BotStates.picked_rates_date)
-async def calendar_handle(callback_query):
-    selected, date = await process_calendar_selection(bot, callback_query)
+@dp.callback_query_handler(calendar_callback.filter(), state=BotStates.picked_rates_date)
+async def calendar_handle(callback_query: CallbackQuery, callback_data: dict):
+    selected, date = await process_calendar_selection(callback_query, callback_data)
     if selected:
-        rate = await getcurrateuah(session=session, date=date.strftime("%Y%m%d"))
-        await bot.send_message(chat_id=callback_query.from_user.id,
-                               text=f'По данным НБУ курс UAH/USD на {date.strftime("%d/%m/%Y")} - {str(rate)}',
-                               reply_markup=start_kb)
-        await BotStates.started.set()
+        if date <= datetime.now():
+            rate = await getcurrateuah(session=session, date=date.strftime("%Y%m%d"))
+            await callback_query.message.answer(f'По данным НБУ курс UAH/USD на {date.strftime("%d/%m/%Y")} - {str(rate)}',
+                                                reply_markup=start_kb)
+            await BotStates.started.set()
+        else:
+            await callback_query.message.answer(f'Невозможно узнать курс, дата из будущего.\nВыберите корректную дату:',
+                                                reply_markup=create_calendar())
 
 
 # picked weather
 @dp.message_handler(state=BotStates.picked_weather)
-async def weather_pick(message: types.Message, state: FSMContext):
+async def weather_pick(message: Message, state: FSMContext):
     c, w = await getweather(session=session, city=message.text)
     await message.answer(f"В городе {c['name_ru']} сейчас {w['weather'][0]['description']}, температура {w['main']['temp']}°C", reply_markup=start_kb)
     await BotStates.started.set()
